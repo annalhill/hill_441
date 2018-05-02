9305165efa1317dc0a44a2332cc6332c2c728cb2
Anna Hill <alouiseh14@gmail.com>
2018-05-01 23:55:08 -0600

    attempt one at a website in javascript- boo

--- HW-6/hw-6.js
+++ HW-6/hw-6.js
@@ -26,3 +26,44 @@ Instead, you should use the appropriate methods, as mentioned in the readings.
 To begin, you should select the body element from the DOM, then remove its existing child nodes. You may then begin to create your web page.
 
 */
+
+body.style.background = "rgb(230, 230, 250)";
+
+var myPic = new Image(1424 × 2131); // Original Dimensions: 2848 × 4272
+myPic.src = "images/myfamily.jpeg"
+
+let body = document.body;
+body.innerText = "";
+
+let elmnt = document.createElement("div");
+let elmnt1 = document.createElement("h1");
+let elmnt2 = document.createElement("h2");
+let elmnt3 = document.createElement("p");
+let elmnt4 = document.createElement("p");
+let elmnt5 = document.createElement("p");
+
+elmnt1.innerText = "Anna Louise Hill";
+elmnt2.innerText = "Homework 6 Coming in VERY late, I am really sorry"
+elmnt3.innerText = "I am not a fan of this class but I am sure it will be useful later in on my life of coding and web design"
+elmnt4.innerText = "Photography is a passion of mine although I am not very good";
+elmnt5.innerText = "Take a look at the gallery on my portfolio website!";
+
+elmnt.setAttribute("style", "text-align: center");
+elmnt1.setAttribute("style","padding:0.5em; margin:0; font-family: american-typewriter; text-align:left; color:#939598");
+elmnt2.setAttribute("style","padding:0em; margin:0; font-weight: normal; font-family: american-typewriter; text-align:left; color:#185c55");
+elmnt3.setAttribute("style","padding:2em; margin:0; font-family: american-typewriter; text-align:left; color:#185c55");
+elmnt4.setAttribute("style","padding:0em; margin:0; font-family: american-typewriter; text-align:left; color:#185c55");
+elmnt5.setAttribute("href", "http://www.annalouisehill.com");
+elmnt5.setAttribute("style", "font-family: american-typewriter; text-decoration: none; color:#fed8a6; border-style: outset");
+
+
+myPic.setAttribute("style", "padding:0em; margin:100px");
+
+elmnt.appendChild(elmnt1);
+elmnt.appendChild(elmnt2);
+elmnt.appendChild(elmnt3);
+elmnt.appendChild(elmnt4);
+elmnt.appendChild(myPic);
+elmnt.appendChild(elmnt5);
+
+body.appendChild(elmnt);
