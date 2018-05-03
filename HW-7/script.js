a3e9578f1a6936a27ed9f72e16dc0e2f93c0c816
Anna Hill <alouiseh14@gmail.com>
2018-05-02 18:30:57 -0600

    I'll figure it out before I die

--- HW-7/script.js
+++ HW-7/script.js
@@ -1,40 +1,112 @@
-body.style.background = "rgb(0)";
-
 let body = document.body;
 body.innerText = "";
+body.style.background = "rgb(255,255,255)";
 
+// Creating header and picture array of elements
 let e = document.createElement("div");
 let e1 = document.createElement("h1");
 let e2 = document.createElement("h2");
 let e3 = document.createElement("p");
+let e4 = document.createElement("p");
+let e5 = document.createElement("p");
 
+// Numbered elements that will correspond to the coffee shop
+let e6 = document.createElement("p");
+let e7 = document.createElement("p");
+let e8 = document.createElement("p");
+let e9 = document.createElement("p");
+let e10 = document.createElement("p");
+let e11 = document.createElement("p");
+let e12 = document.createElement("p");
+let e13 = document.createElement("p");
+let e14 = document.createElement("p");
+let e15 = document.createElement("p");
+let e16 = document.createElement("p");
+let e17 = document.createElement("p");
+let e18 = document.createElement("p");
+let e19 = document.createElement("p");
+let e20 = document.createElement("p");
+let e21 = document.createElement("p");
+let e22 = document.createElement("p");
+let e23 = document.createElement("p");
+let e24 = document.createElement("p");
+let e25 = document.createElement("p");
+let e26 = document.createElement("p");
 
+// Header text
 e1.innerText = "Coffee Talk";
-e2.innerText = "This is an infographic adventure to show you just how many coffee shop options are in Missoula for you!"
-e3.innerText = "Click on the button in reference to the dots and learn more!"
+e2.innerText = "This is an infographic adventure to show you just how many coffee shop options are in Missoula for you!";
+e3.innerText = "Click on the button in reference to the dots and learn more!";
+e5.innerText = "Check out more of my work here!";
 
-e5.innerText = "Check out more of my work here!"
+// Numbered elements that will correspond to the coffee shop- In actual writing
+e6.innerText = "1";
+e7.innerText = "2";
+e8.innerText = "3";
+e9.innerText = "4";
+e10.innerText = "5";
+e11.innerText = "6";
+e12.innerText = "7";
+e13.innerText = "8";
+e14.innerText = "9";
+e15.innerText = "10";
+e16.innerText = "11";
+e17.innerText = "12";
+e18.innerText = "13";
+e19.innerText = "14";
+e20.innerText = "15";
+e21.innerText = "16";
+e22.innerText = "17";
+e23.innerText = "18";
+e24.innerText = "19";
+e25.innerText = "20";
+e26.innerText = "21";
 
 
 e.setAttribute("style", "text-align: center");
-e1.setAttribute("style","padding:0.5em; margin:0; font-family: american-typewriter; text-align:left; color:#939598");
-e2.setAttribute("style","padding:0em; margin:0; font-weight: normal; font-family: american-typewriter; text-align:left; color:#185c55");
-e3.setAttribute("style","padding:2em; margin:0; font-family: american-typewriter; text-align:left; color:#185c55");
+e1.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e2.setAttribute("style","padding:0em; margin:0; font-weight: normal; font-family: american-typewriter; text-align:center; color:#185c55");
+e3.setAttribute("style","padding:0em; margin:0; font-family: american-typewriter; text-align:center; color:#185c55");
 // e4.setAttribute("style","padding:0em; margin:0; font-family: american-typewriter; text-align:left; color:#185c55");
 e5.setAttribute("href", "http://www.annalouisehill.com");
 e5.setAttribute("style", "font-family: american-typewriter; text-decoration: none; color:#fed8a6; border-style: outset");
 
-var myPic = new Image(3306 × 3306); // Original Dimensions: 3306 x 3306
-myPic.src = "images/Coffee_Talk_Image_Numbers.jpeg"
+e6.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e7.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e8.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e9.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e10.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e11.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e12.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e13.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e14.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e15.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e16.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e17.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e18.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e19.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e20.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e21.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e22.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e23.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e24.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e25.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
+e26.setAttribute("style","padding:0em; margin:0; font-weight: bold; font-family: american-typewriter; text-align:center; color:#939598");
 
-myPic.setAttribute("style", "padding:0em; margin:100px");
+var myPic1 = new Image(1650, 200); // Original Dimensions: 3306 x 3306
+myPic1.src = "./images/BP.jpg"
+
+var myPic2 = new Image(1102, 1102); // Original Dimensions: 3306 x 3306
+myPic2.src = "./images/Coffee_Talk_Image_Numbers.jpg"
+
+myPic1.setAttribute("style", "padding:0em; margin:10px");
+myPic2.setAttribute("style", "padding:0em; margin:100px");
 
 e.appendChild(e1);
 e.appendChild(e2);
+e.appendChild(myPic1);
 e.appendChild(e3);
-e.appendChild(e4);
-e.appendChild(myPic);
-
+e.appendChild(myPic2);
 e.appendChild(e5);
 
 body.appendChild(e);
