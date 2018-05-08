7f563f6a78eaf15e948860e4f047e1f14ec36ffe
Anna Hill <alouiseh14@gmail.com>
2018-05-08 12:51:25 -0600

    new information

--- HW-8/sketch.js
+++ HW-8/sketch.js
@@ -1,23 +1,41 @@
-// Create request
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = './images/logo.jpg';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
 var request = new XMLHttpRequest();
-
-// URL connection
 request.open('GET', 'https://www.nationalpark-adventures.com/united-states-national-parks.html', true);
+request.onload = function () {
 
-request.onload = function() {
-  // Data
-  console.log("recieved", response);
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(park => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = park.name;
+
+      const p = document.createElement('p');
+      park.description = park.description.substring(0, 300);
+      p.textContent = `${park.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
 }
 
-
-// Send request
 request.send();
-
-// Data
-//JSON.parse reads data from URL
-var data = JSON.parse(this.response);
-
-data.forEach(park => {
-  // Log each movie's title
-  console.log(park.name);
-});
